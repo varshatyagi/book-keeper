805525825a0cc8dab298469ccaa9562ee566d9b9
Varsha <varsha@newput.com>
2018-05-04 16:17:45 +0530

    Integrate user authentication and token expiry

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'puma', '~> 3.0'
 gem 'rack-cors'
 gem 'devise'
 gem 'rails_admin', '~> 1.3'
+gem 'jwt'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,39 @@
 class ApplicationController < ActionController::API
-  require 'http-status'
+  require 'error_codes'
+  require 'auth'
+
+  before_action :authenticate
+
+  def authenticate
+    auth_present ? true : (render json: ApiResponse::ACCESS_DENIED, status: ApiResponse::HTTP_CODE[:UNAUTHORIZE])
+  end
+
+  private
+    def auth_present
+      if request.env.fetch("HTTP_AUTHORIZATION", "").scan(/Bearer/).flatten.first === 'Bearer'
+        checkAuthentication
+      else
+        return false
+      end
+    end
+
+    def checkAuthentication
+      token = request.env["HTTP_AUTHORIZATION"].scan(/Bearer(.*)$/).flatten.last.squish
+      if token && Auth.decode(token)
+        decodedUserId = Auth.decode(token)
+        currentUser = getCurrentUser(userId: decodedUserId["user"])
+        isTokenValid(token: token, user: currentUser)
+      else
+        return false
+      end
+    end
+
+    def getCurrentUser(userId: userId)
+      return User.find(userId)
+    end
+
+    def isTokenValid(token: token, user: currentUser)
+      token === user[:token] ? !((DateTime.now.to_i - user[:reset_password_token_at].to_i) > ApiResponse::TOKEN_EXPIRATION_TIME) : false
+    end
+
 end
