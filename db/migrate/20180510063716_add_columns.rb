d424cb563592439889922afda8a3a75080eb3503
Varsha <varsha@newput.com>
2018-05-11 15:06:41 +0530

    changeRequestOnMigration

--- db/migrate/20180510063716_add_columns.rb
+++ db/migrate/20180510063716_add_columns.rb
@@ -2,6 +2,6 @@ class AddColumns < ActiveRecord::Migration[5.0]
   def change
     add_column :org_bank_accounts, :bank_balance, :decimal
     add_column :transactions, :bank_id, :integer
-    change_column :transactions, :ledger_heading, 'integer USING CAST(column_name AS integer)'
+    change_column :transactions, :ledger_heading, 'integer USING CAST(ledger_heading AS integer)'
   end
 end
