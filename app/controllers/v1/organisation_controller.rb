8fc12e4f83f711e1bb01f1ffcf53531f3b61a70d
Varsha <varsha@newput.com>
2018-05-14 15:28:35 +0530

    get-api-organisation

--- app/controllers/v1/organisation_controller.rb
+++ app/controllers/v1/organisation_controller.rb
@@ -1,5 +1,6 @@
 class V1::OrganisationController < ApplicationController
 
+  before_action :authenticate
   def getOrganisationMoneyBalance
     helper = Helper.new
     consolidatedBalance = OrgBalance.find_by(org_id: params[:orgId])
@@ -8,5 +9,15 @@ class V1::OrganisationController < ApplicationController
       return true
     end
     render json: helper.returnSuccessResponse(obj: consolidatedBalance)
+  end
+
+  def organisation
+    helper = Helper.new
+    organisation = Organisation.find_by(id: params[:orgId])
+    unless organisation
+      render json: helper.returnErrorResponse(errors: nil), status: Helper::HTTP_CODE[:BAD_REQUEST]
+      return true
+    end
+    render json: helper.returnSuccessResponse(obj: organisation)
   end
 end
