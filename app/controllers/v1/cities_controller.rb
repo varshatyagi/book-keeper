0b120e44cfc267d713e1edeb8c38c014da6ac4f9
Varsha <varsha@newput.com>
2018-05-28 12:03:15 +0530

    city

--- app/controllers/v1/cities_controller.rb
+++ app/controllers/v1/cities_controller.rb
@@ -2,7 +2,6 @@ class V1::CitiesController < ApplicationController
 
   def index
     city_scope = City.all
-    byebug
     city_scope = city_scope.where(state_code: params[:state_code].upcase) if params[:state_code].present?
     cities = city_scope
     cities = cities.map {|city| CitySerializer.new(city).serializable_hash} if cities.present?
