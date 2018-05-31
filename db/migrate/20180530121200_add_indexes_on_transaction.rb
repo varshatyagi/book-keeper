9dde62b0d9ad8d2e160a4f6571359b1b4ceb5890
Varsha <varsha@newput.com>
2018-05-31 15:41:20 +0530

    fix-migration

--- db/migrate/20180530121200_add_indexes_on_transaction.rb
+++ db/migrate/20180530121200_add_indexes_on_transaction.rb
@@ -4,7 +4,7 @@ class AddIndexesOnTransaction < ActiveRecord::Migration[5.0]
     add_index :transactions, :ledger_heading_id
     add_foreign_key :transactions, :ledger_headings
     add_foreign_key :transactions, :organisations
-    add_foreign_key :transactions, :org_bank_account_ids
+    add_foreign_key :transactions, :org_bank_account_id
     add_foreign_key :transactions, :alliances
   end
 end
