b0e1ff8fdb7656711fa5c08d4abad66e205dc60c
Varsha <varsha@newput.com>
2018-06-06 09:27:50 +0530

    not-column-found

--- db/migrate/20180531103134_add_org_bank_account_balance_summary.rb
+++ db/migrate/20180531103134_add_org_bank_account_balance_summary.rb
@@ -9,7 +9,6 @@ class AddOrgBankAccountBalanceSummary < ActiveRecord::Migration[5.0]
     end
 
     add_foreign_key :org_bank_account_balance_summaries, :org_bank_accounts
-    add_index :org_bank_account_balance_summaries, :financial_year
 
     remove_column :org_bank_accounts, :initial_balance
     remove_column :org_bank_accounts, :bank_balance
