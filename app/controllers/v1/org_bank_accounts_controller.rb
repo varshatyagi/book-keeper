0a539d7ea32007763c21493eb13f96b411ef1b21
Varsha <varsha@newput.com>
2018-06-07 16:30:17 +0530

    Added-balance-summary

--- app/controllers/v1/org_bank_accounts_controller.rb
+++ app/controllers/v1/org_bank_accounts_controller.rb
@@ -1,6 +1,6 @@
 class V1::OrgBankAccountsController < ApplicationController
 
-  # before_action :require_user
+  before_action :require_user
   # before_action :require_admin_or_organisation_owner
 
   def index
