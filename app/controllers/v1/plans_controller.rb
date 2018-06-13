acf82192a357289edd7ddcb9257d74934940bd03
Varsha <varsha@newput.com>
2018-06-13 11:08:10 +0530

    plan name added in json

--- app/controllers/v1/plans_controller.rb
+++ app/controllers/v1/plans_controller.rb
@@ -1,7 +1,7 @@
 class V1::PlansController < ApplicationController
 
-  #before_action :require_user
-  #before_action :require_admin, only: [:create, :update]
+  before_action :require_user
+  before_action :require_admin, only: [:create, :update]
 
   def create
     organisation = Organisation.find_by(id: params[:organisation_id]) || not_found
@@ -22,10 +22,9 @@ class V1::PlansController < ApplicationController
 
   def update
     plan = Plan.find(params[:id]) || not_found
-    plan[:plan_end_date] = plan[:plan_start_date] + 1.year
     organisation = Organisation.find(params[:organisation_id])
     ApplicationRecord.transaction do
-      plan.save!
+      plan.update_attributes!(plan_params)
       organisation.update_attributes!(active_plan_id: plan.plan)
     end
     render json: {response: PlanSerializer.new(plan).serializable_hash}
@@ -48,12 +47,13 @@ class V1::PlansController < ApplicationController
   end
 
   def user_signup_via_email(user)
-    OrganizationNotifierMailer.plan_activated(user).deliver
+    password = Common.generate_string
     user.update_attributes!({
       status: User::USER_STATUS_ACTIVE,
-      password: Common.generate_string,
-      password_confirmation: Common.generate_string,
+      password: password,
+      password_confirmation: password,
       is_temporary_password: true
     })
+    OrganizationNotifierMailer.plan_activated(user, password).deliver
   end
 end
--- app/serializers/organisation_serializer.rb
+++ app/serializers/organisation_serializer.rb
@@ -1,6 +1,6 @@
 class OrganisationSerializer < ActiveModel::Serializer
-  attributes :id, :name, :owner_id, :created_by, :owner, :is_setup_complete, :plan_info, :preferred_plan_id
-  attributes :active_plan_id, :created_at, :balance_summary
+  attributes :id, :name, :owner_id, :created_by, :is_setup_complete, :owner, :preferred_plan_id
+  attributes :active_plan_id, :created_at, :balance_summary, :plan_info
 
   def owner
     return nil if object.id.blank?
@@ -15,21 +15,21 @@ class OrganisationSerializer < ActiveModel::Serializer
     end
     preferred_id = object.preferred_plan_id
     if object.user.status == User::USER_STATUS_PENDING
-      return {active_plan_id: active_id}
+      plan_info = {active_plan_id: active_id}
     elsif object.user.status == User::USER_STATUS_ACTIVE
       plan_detail = object.plan
       # if plan end date is smaller than current time than expired flag is true
       expired = plan_detail && plan_detail.plan_end_date ? plan_detail.plan_end_date < DateTime.now : nil
+      plan_info = { id: plan_detail ? plan_detail.id : nil,
+                    active_plan_id: active_id ? active_id : nil,
+                    active_plan_name: active_id ? Plan::PLAN_NAME[active_id] : nil,
+                    plan_start_date: plan_detail && plan_detail.plan_start_date ? plan_detail.plan_start_date : nil,
+                    plan_end_date: plan_detail && plan_detail.plan_end_date ? plan_detail.plan_end_date : nil,
+                    amount: plan_detail && plan_detail.amount.to_f ? plan_detail.amount.to_f : nil,
+                    expired: expired
+                  }
     end
-      return {
-              id: plan_detail ? plan_detail.id : nil,
-              active_plan_id: active_id ? active_id : nil,
-              active_plan_name: active_id ? Plan::PLAN_NAME[active_id] : nil,
-              plan_start_date: plan_detail && plan_detail.plan_start_date ? plan_detail.plan_start_date : nil,
-              plan_end_date: plan_detail && plan_detail.plan_end_date ? plan_detail.plan_end_date : nil,
-              amount: plan_detail && plan_detail.amount.to_f ? plan_detail.amount.to_f : nil,
-              expired: expired
-            }
+      return plan_info
   end
 
   def balance_summary
