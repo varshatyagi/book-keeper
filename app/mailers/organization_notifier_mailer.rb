0c09191a5719b0d6f3dfb8f230c11817026e32d1
Varsha <varsha@newput.com>
2018-06-13 11:55:25 +0530

    add correct email id to mailer

--- app/mailers/organization_notifier_mailer.rb
+++ app/mailers/organization_notifier_mailer.rb
@@ -4,17 +4,17 @@ class OrganizationNotifierMailer < ApplicationMailer
   admin_email = User.find_by(role: 'admin').email
 
   def thank_you_email(user)
-    mail(to: 'varsha@newput.com', subject: 'Thank you for contacting Onacc')
+    mail(:to => user.email, :subject => 'Thank you for contacting Onacc')
   end
 
   def activate_user(user)
     @user = user
-    mail(to: 'varsha@newput.com', subject: 'Request to Activate Plan')
+    mail(:to => admin_email, :subject => 'Request to Activate Plan')
   end
 
   def plan_activated(user, password)
     @user = user
     @password = password
-    mail(to: 'varsha@newput.com', subject: 'Your Onacc a/c is active now!')
+    mail(:to => user.email, :subject => 'Your Onacc a/c is active now!')
   end
 end
