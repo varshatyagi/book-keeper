989dd7188a16204e369b7d7cfa37f1f841e8cb1f
Varsha <varsha@newput.com>
2018-06-14 14:06:29 +0530

    Migration changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180613131238) do
+ActiveRecord::Schema.define(version: 20180614070823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 20180613131238) do
     t.decimal  "opening_balance"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.datetime "financial_year"
   end
 
   create_table "org_bank_accounts", force: :cascade do |t|
@@ -123,7 +124,6 @@ ActiveRecord::Schema.define(version: 20180613131238) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.datetime "opening_date"
-    t.datetime "financial_year"
   end
 
   create_table "organisations", force: :cascade do |t|
